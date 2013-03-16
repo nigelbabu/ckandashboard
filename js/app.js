c72f6bb7b92fdb06534f981008c37d8c48043ba6
Nigel Babu <nigelbabu@gmail.com>
2013-03-16 18:28:46 +0530

    Add mustachify and change assigned to pull req

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 (function () {
+    var template = '<tr><td><a href="{{url}}">{{ number }}</a></td><td>{{title}}</td><td>{{ #pull_request.html_url }}<a href="{{ pull_request.html_url }}">Pull Request</a>{{ /pull_request.html_url }}</td><td>{{ updated_at }}</td></tr>';
     var tbl = $('#issues');
-    $.getJSON('https://api.github.com/repos/okfn/ckan/issues?assignee=none&sort=updated&callback=?', function(resp) {
+    $.getJSON('https://api.github.com/repos/okfn/ckan/issues?assignee=none&sort=updated&direction=asc&callback=?', function(resp) {
         $.each(resp['data'], function(index, row) {
-            console.log(row);
-            $('<tr><td colspan="4">' + row['number'] + '</td></tr>').appendTo(tbl);
+            $(Mustache.render(template, row)).appendTo(tbl);
         });
     });
 
